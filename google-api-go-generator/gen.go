f9c247ce0b039c5e6745c5d2a13fbd27bc2a1954
Glenn Lewis <gmlewis@google.com>
2015-05-11 14:53:00 -0700

    google-api-go-client: strip `" and "` from comments
    This fixes issue #80.
    It turns out that the only APIs with these kind of comments
    were the cloudsearch:v1 and logging:v1beta3 APIs.
    
    Change-Id: I2ebbcaa8c49ed23a4f35a52075d213b6a947b956

--- google-api-go-generator/gen.go
+++ google-api-go-generator/gen.go
@@ -1685,13 +1685,14 @@ func (a *arguments) String() string {
 func asComment(pfx, c string) string {
 	var buf bytes.Buffer
 	const maxLen = 70
-	removeNewlines := func(s string) string {
-		return strings.Replace(s, "\n", "\n"+pfx+"// ", -1)
+	removeStuff := func(s string) string {
+		r := strings.NewReplacer("\n", "\n"+pfx+"// ", "`\"", `"`, "\"`", `"`)
+		return r.Replace(s)
 	}
 	for len(c) > 0 {
 		line := c
 		if len(line) < maxLen {
-			fmt.Fprintf(&buf, "%s// %s\n", pfx, removeNewlines(line))
+			fmt.Fprintf(&buf, "%s// %s\n", pfx, removeStuff(line))
 			break
 		}
 		line = line[:maxLen]
@@ -1702,7 +1703,7 @@ func asComment(pfx, c string) string {
 		if si != -1 {
 			line = line[:si]
 		}
-		fmt.Fprintf(&buf, "%s// %s\n", pfx, removeNewlines(line))
+		fmt.Fprintf(&buf, "%s// %s\n", pfx, removeStuff(line))
 		c = c[len(line):]
 		if si != -1 {
 			c = c[1:]

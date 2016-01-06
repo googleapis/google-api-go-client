52ece9b8dc561f8ba70cc2ad17d958bc06fb601a
Michael McGreevy <mcgreevy@golang.org>
2016-01-06 14:23:34 +1100

    google-api-go-client/all: Add content sniffer.
    This adds a wrapper around io.Reader which sniffs the Reader's media
    type without the use of pipes & goroutines.
    
    Various other content-sniffing code in gensupport will be refactored in
    a subsequent change to remove the pipe/goroutine-based code.
    
    Change-Id: Ic1260fa28ea39116a0531a333edd49679ab8e9e8
    Reviewed-on: https://code-review.googlesource.com/3973
    Reviewed-by: Dave Day <djd@golang.org>

--- gensupport/media.go
+++ gensupport/media.go
@@ -17,6 +17,60 @@ import (
 	"google.golang.org/api/googleapi"
 )
 
+const sniffBuffSize = 512
+
+func NewContentSniffer(r io.Reader) *ContentSniffer {
+	return &ContentSniffer{r: r}
+}
+
+// ContentSniffer wraps a Reader, and reports the content type determined by sniffing up to 512 bytes from the Reader.
+type ContentSniffer struct {
+	r     io.Reader
+	start []byte // buffer for the sniffed bytes.
+	err   error  // set to any error encountered while reading bytes to be sniffed.
+
+	ctype   string // set on first sniff.
+	sniffed bool   // set to true on first sniff.
+}
+
+func (sct *ContentSniffer) Read(p []byte) (n int, err error) {
+	// Ensure that the content type is sniffed before any data is consumed from Reader.
+	_, _ = sct.ContentType()
+
+	if len(sct.start) > 0 {
+		n := copy(p, sct.start)
+		sct.start = sct.start[n:]
+		return n, nil
+	}
+
+	// We may have read some bytes into start while sniffing, even if the read ended in an error.
+	// We should first return those bytes, then the error.
+	if sct.err != nil {
+		return 0, sct.err
+	}
+
+	// Now we have handled all bytes that were buffered while sniffing.  Now just delegate to the underlying reader.
+	return sct.r.Read(p)
+}
+
+// ContentType returns the sniffed content type, and whether the content type was succesfully sniffed.
+func (sct *ContentSniffer) ContentType() (string, bool) {
+	if sct.sniffed {
+		return sct.ctype, sct.ctype != ""
+	}
+	sct.sniffed = true
+	// If ReadAll hits EOF, it returns err==nil.
+	sct.start, sct.err = ioutil.ReadAll(io.LimitReader(sct.r, sniffBuffSize))
+
+	// Don't try to detect the content type based on possibly incomplete data.
+	if sct.err != nil {
+		return "", false
+	}
+
+	sct.ctype = http.DetectContentType(sct.start)
+	return sct.ctype, true
+}
+
 // ConditionallyIncludeMedia does nothing if media is nil.
 //
 // bodyp is an in/out parameter.  It should initially point to the

00ccdf74c4d0e49b0b069fc7a6832a56bee0b913
Michael McGreevy <mcgreevy@golang.org>
2016-01-14 14:56:10 +1100

    googleapi: Refactor ResumableUpload to use ResumableBuffer.
    Also cleaned up some tests along the way.
    
    Change-Id: I9537f403951103b8b9a42eb348079f117230b274

--- groupsmigration/v1/groupsmigration-gen.go
+++ groupsmigration/v1/groupsmigration-gen.go
@@ -261,12 +261,14 @@ func (c *ArchiveInsertCall) Do() (*Groups, error) {
 		return nil, err
 	}
 	if c.protocol_ == "resumable" {
+		chunkSize := 1 << 23
 		loc := res.Header.Get("Location")
+		mediaReader := gensupport.ReaderAtToReader(c.resumable_, c.resumable_.Size())
 		rx := &gensupport.ResumableUpload{
 			Client:        c.s.client,
 			UserAgent:     c.s.userAgent(),
 			URI:           loc,
-			Media:         c.resumable_,
+			Media:         gensupport.NewResumableBuffer(mediaReader, chunkSize),
 			MediaType:     c.resumableMediaType_,
 			ContentLength: c.resumable_.Size(),
 			Callback: func(curr int64) {

d2bfc4131a034197ecc039fe5326701001e90254
Dave Day <djd@golang.org>
2016-02-15 15:21:21 +1100

    gensupport: add a helper Retry function
    This function invokes the given function a number of times using the
    same logic as used by ResumableUpload.Upload. I'm introducing it to add
    similar retry logic to single-request uploads too for #179.
    
    While I'm here, simplify NoPauseStrategy.
    
    Change-Id: I8c5247bcba5bb704fc2e35dec4f3b61c6b0b59e9
    Reviewed-on: https://code-review.googlesource.com/4173
    Reviewed-by: Michael McGreevy <mcgreevy@golang.org>

--- gensupport/util_test.go
+++ gensupport/util_test.go
@@ -27,11 +27,31 @@ func (er *errReader) Read(p []byte) (int, error) {
 	return n, nil
 }
 
-// NoPauseStrategy implements BackoffStrategy with infinite 0-length pauses.
-type NoPauseStrategy struct{}
+// UniformPauseStrategy implements BackoffStrategy with uniform pause.
+type UniformPauseStrategy time.Duration
 
-func (np NoPauseStrategy) Pause() (time.Duration, bool) {
-	return 0, true
+func (p UniformPauseStrategy) Pause() (time.Duration, bool) { return time.Duration(p), true }
+func (p UniformPauseStrategy) Reset()                       {}
+
+// NoPauseStrategy implements BackoffStrategy with infinite 0-length pauses.
+const NoPauseStrategy = UniformPauseStrategy(0)
+
+// LimitRetryStrategy wraps a BackoffStrategy but limits the number of retries.
+type LimitRetryStrategy struct {
+	Max      int
+	Strategy BackoffStrategy
+	n        int
 }
-func (np NoPauseStrategy) Reset() {
+
+func (l *LimitRetryStrategy) Pause() (time.Duration, bool) {
+	l.n++
+	if l.n > l.Max {
+		return 0, false
+	}
+	return l.Strategy.Pause()
+}
+
+func (l *LimitRetryStrategy) Reset() {
+	l.n = 0
+	l.Strategy.Reset()
 }

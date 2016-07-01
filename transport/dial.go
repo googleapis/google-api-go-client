c13a21ee847eca050f08db8373d8737494a1170e
Jonathan Amsterdam <jba@google.com>
2016-07-01 14:13:47 -0400

    transport: apply latest changes from gocloud transport
    Update this transport package with the recent changes to
    code.googlesource.com/gocloud's internal/transport package.
    
    Also rename transport.go to dial.go to match that other package, and add
    license boilerplate to it.
    
    Change-Id: I714bf6c4b7c91de373ff838c23a9481b1ba13859
    Reviewed-on: https://code-review.googlesource.com/5141
    Reviewed-by: Dave Day <djd@golang.org>

--- transport/dial.go
+++ transport/dial.go
@@ -1,3 +1,17 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package transport supports network connections to HTTP and GRPC servers.
 // This package is not intended for use by end developers. Use the
 // google.golang.org/api/option package to configure API clients.
@@ -44,6 +58,9 @@ func NewHTTPClient(ctx context.Context, opts ...option.ClientOption) (*http.Clie
 	return oauth2.NewClient(ctx, o.TokenSource), o.Endpoint, nil
 }
 
+// Set at init time by dial_appengine.go. If nil, we're not on App Engine.
+var appengineDialerHook func(context.Context) grpc.DialOption
+
 // DialGRPC returns a GRPC connection for use communicating with a Google cloud
 // service, configured with the given ClientOptions.
 func DialGRPC(ctx context.Context, opts ...option.ClientOption) (*grpc.ClientConn, error) {
@@ -67,6 +84,10 @@ func DialGRPC(ctx context.Context, opts ...option.ClientOption) (*grpc.ClientCon
 	grpcOpts := []grpc.DialOption{
 		grpc.WithPerRPCCredentials(oauth.TokenSource{o.TokenSource}),
 		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, "")),
+	}
+	if appengineDialerHook != nil {
+		// Use the Socket API on App Engine.
+		grpcOpts = append(grpcOpts, appengineDialerHook(ctx))
 	}
 	grpcOpts = append(grpcOpts, o.GRPCDialOpts...)
 	if o.UserAgent != "" {

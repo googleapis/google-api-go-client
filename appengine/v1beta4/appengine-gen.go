2a5d4c86566aea5db0ee52e847408b566e23fcae
Dave Day <djd@golang.org>
2016-08-05 14:23:27 +1000

    all: rename the top "Service" type if it conflicts with an API type.
    For specific APIs, we use "APIService" instead. Add a check to detect
    if new APIs/types begin to clash with the service name.
    
    Change-Id: Ie78e859f761b5729088af589f20bcab168da1e0a

--- appengine/v1beta4/appengine-gen.go
+++ appengine/v1beta4/appengine-gen.go
@@ -51,16 +51,16 @@ const (
 	CloudPlatformScope = "https://www.googleapis.com/auth/cloud-platform"
 )
 
-func New(client *http.Client) (*Service, error) {
+func New(client *http.Client) (*APIService, error) {
 	if client == nil {
 		return nil, errors.New("client is nil")
 	}
-	s := &Service{client: client, BasePath: basePath}
+	s := &APIService{client: client, BasePath: basePath}
 	s.Apps = NewAppsService(s)
 	return s, nil
 }
 
-type Service struct {
+type APIService struct {
 	client    *http.Client
 	BasePath  string // API endpoint base URL
 	UserAgent string // optional additional User-Agent fragment
@@ -68,7 +68,7 @@ type Service struct {
 	Apps *AppsService
 }
 
-func (s *Service) userAgent() string {
+func (s *APIService) userAgent() string {
 	if s.UserAgent == "" {
 		return googleapi.UserAgent
 	}

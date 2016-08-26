68fe7fb453dc9aa330671e6f737c693e5248c256
Jonathan Amsterdam <jba@google.com>
2016-08-26 13:19:04 -0400

    iterator: more extensive tests
    Add a fake service that exhibits many valid behaviors, and test against
    various possibilities.
    
    Also, minor doc improvements.
    
    Change-Id: I06f359132b6c2337db83ddec145a0dbf2cd53967

--- iterator/iterator_test.go
+++ iterator/iterator_test.go
@@ -15,43 +15,120 @@
 package iterator_test
 
 import (
+	"encoding/json"
+	"math"
 	"reflect"
-	"strconv"
 	"testing"
 
 	"golang.org/x/net/context"
 	"google.golang.org/api/iterator"
 )
 
+// Service represents the implementation of a Google API's List method.
+// We want to test against a large range of possible valid behaviors.
+// All the behaviors this can generate are valid under the spec for
+// Google API paging.
 type service struct {
+	// End of the sequence. end-1 is the last value returned.
 	end int
+	// Maximum number of items to return in one RPC. Also the default page size.
+	// If zero, max is unlimited.
+	max int
+	// If true, return two empty pages before each RPC that returns items, and
+	// two zero pages at the end. E.g. if end = 5, max = 2 and the pageSize
+	// parameter to List is zero, then the number of items returned in
+	// successive RPCS is:
+	//    0 0 2 0 0 2 0 0 1 0 0
+	// Note that this implies that the RPC returning the last items will have a
+	// non-empty page token.
+	zeroes bool
 }
 
-// List simulates an API List RPC.
+// List simulates an API List RPC. It returns integers in the range [0, s.end).
 func (s *service) List(pageSize int, pageToken string) ([]int, string, error) {
-	// Never give back any more than 3.
-	if pageSize <= 0 || pageSize > 3 {
-		pageSize = 3
+	max := s.max
+	if max == 0 {
+		max = math.MaxInt64
 	}
-	var start int
+	// Never give back any more than s.max.
+	if pageSize <= 0 || pageSize > max {
+		pageSize = max
+	}
+	state := &listState{}
 	if pageToken != "" {
-		var err error
-		start, err = strconv.Atoi(pageToken)
-		if err != nil {
+		if err := json.Unmarshal([]byte(pageToken), state); err != nil {
 			return nil, "", err
 		}
 	}
-	var ints []int
-	var i int
-	for i = start; i < s.end && len(ints) < pageSize; i++ {
-		ints = append(ints, i)
-	}
-	if i == s.end {
+	ints := state.advance(pageSize, s.end, s.zeroes)
+	if state.Start == s.end && (!s.zeroes || state.NumZeroes == 2) {
 		pageToken = ""
 	} else {
-		pageToken = strconv.Itoa(i)
+		bytes, err := json.Marshal(state)
+		if err != nil {
+			return nil, "", err
+		}
+		pageToken = string(bytes)
 	}
 	return ints, pageToken, nil
+}
+
+type listState struct {
+	Start     int // where to start this page
+	NumZeroes int // number of consecutive empty pages before this
+}
+
+func (s *listState) advance(pageSize, end int, zeroes bool) []int {
+	var page []int
+	if zeroes && s.NumZeroes != 2 {
+		// Return a zero page.
+	} else {
+		for i := s.Start; i < end && len(page) < pageSize; i++ {
+			page = append(page, i)
+		}
+	}
+	s.Start += len(page)
+	if len(page) == 0 {
+		s.NumZeroes++
+	} else {
+		s.NumZeroes = 0
+	}
+	return page
+}
+
+func TestServiceList(t *testing.T) {
+	for _, test := range []struct {
+		svc      service
+		pageSize int
+		want     [][]int
+	}{
+		{service{end: 0}, 0, [][]int{nil}},
+		{service{end: 5}, 0, [][]int{{0, 1, 2, 3, 4}}},
+		{service{end: 5}, 8, [][]int{{0, 1, 2, 3, 4}}},
+		{service{end: 5}, 2, [][]int{{0, 1}, {2, 3}, {4}}},
+		{service{end: 5, max: 2}, 0, [][]int{{0, 1}, {2, 3}, {4}}},
+		{service{end: 5, max: 2}, 1, [][]int{{0}, {1}, {2}, {3}, {4}}},
+		{service{end: 5, max: 2}, 10, [][]int{{0, 1}, {2, 3}, {4}}},
+		{service{end: 5, zeroes: true}, 0, [][]int{nil, nil, {0, 1, 2, 3, 4}, nil, nil}},
+		{service{end: 5, max: 3, zeroes: true}, 0, [][]int{nil, nil, {0, 1, 2}, nil, nil, {3, 4}, nil, nil}},
+	} {
+		var got [][]int
+		token := ""
+		for {
+			items, nextToken, err := test.svc.List(test.pageSize, token)
+			if err != nil {
+				t.Fatalf("%v, %d: %v", test.svc, test.pageSize, err)
+			}
+			got = append(got, items)
+			if nextToken == "" {
+				break
+			}
+			token = nextToken
+		}
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%v, %d: got %v, want %v", test.svc, test.pageSize, got, test.want)
+		}
+	}
 }
 
 type Client struct{ s *service }
@@ -64,7 +141,7 @@ type ItemIterator struct {
 	items    []int
 }
 
-// Page returns a PageInfo, which supports pagination.
+// PageInfo returns a PageInfo, which supports pagination.
 func (it *ItemIterator) PageInfo() *iterator.PageInfo { return it.pageInfo }
 
 // Items is a sample implementation of an iterator-creating method.
@@ -92,44 +169,88 @@ func (it *ItemIterator) Next() (int, error) {
 	return item, nil
 }
 
-func TestIterator(t *testing.T) {
-	client := &Client{&service{end: 10}}
-	it := client.Items(ctx)
-	for i := 0; i < 10; i++ {
-		n, err := it.Next()
-		if err != nil {
-			t.Fatalf("%d: got %v, want nil", i, err)
+func TestNext(t *testing.T) {
+	// Test the iterator's Next method with a variety of different service behaviors.
+	// This is primarily a test of PageInfo.next.
+	for _, svc := range []service{
+		{end: 0},
+		{end: 5},
+		{end: 5, max: 1},
+		{end: 5, max: 2},
+		{end: 5, zeroes: true},
+		{end: 5, max: 2, zeroes: true},
+	} {
+		client := &Client{&svc}
+		it := client.Items(ctx)
+		var got []int
+		for {
+			item, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				t.Fatalf("%v: got %v, want nil", svc, err)
+			}
+			got = append(got, item)
 		}
-		if got, want := n, i; got != want {
-			t.Fatalf("got %d, want %d", got, want)
+		want := seq(0, svc.end)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("%v: got %v, want %v", svc, got, want)
 		}
-	}
-	_, err := it.Next()
-	if err != iterator.Done {
-		t.Fatalf("end: got %v, want Done", err)
+		// Confirm that Next continues to return (0, Done).
+		for i := 0; i < 3; i++ {
+			item, err := it.Next()
+			if item != 0 || err != iterator.Done {
+				t.Errorf("%s, after end: got (%d, %v), want (0, Done)", svc, item, err)
+			}
+		}
 	}
 }
 
+// TODO(jba): test setting PageInfo.MaxSize
+// TODO(jba): test setting PageInfo.Token
+
 func TestPager(t *testing.T) {
 	const pageSize = 4
-	client := &Client{&service{end: 11}}
-	it := client.Items(ctx)
-	p := iterator.NewPager(it, pageSize, "")
-	for i := 0; i < client.s.end; i += pageSize {
-		var items []int
-		tok, err := p.NextPage(&items)
-		if err != nil {
-			t.Fatal(err)
+	for _, svc := range []service{
+		{end: 0},
+		{end: 3},
+		{end: 3, zeroes: true},
+		{end: 11},
+		{end: 11, max: 2},
+		{end: 11, zeroes: true},
+		{end: 11, max: 2, zeroes: true},
+	} {
+		var want [][]int
+		switch svc.end {
+		case 0:
+			want = nil
+		case 3:
+			want = [][]int{{0, 1, 2}}
+		case 11:
+			want = [][]int{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10}}
+		default:
+			t.Fatalf("unexpected svc.end %d", svc.end)
 		}
-		end := i + pageSize
-		if end > client.s.end {
-			end = client.s.end
+		client := &Client{&svc}
+		it := client.Items(ctx)
+		p := iterator.NewPager(it, pageSize, "")
+		var got [][]int
+		for {
+			var items []int
+			tok, err := p.NextPage(&items)
+			if err != nil {
+				t.Fatalf("%v: %v", svc, err)
+			}
+			if tok != "" || len(items) != 0 { // edge case: 0-length sequence
+				got = append(got, items)
+			}
+			if tok == "" {
+				break
+			}
 		}
-		if got, want := items, seq(i, end); !reflect.DeepEqual(got, want) {
-			t.Fatalf("got %v, want %v", got, want)
-		}
-		if (tok == "") != (i == 8) {
-			t.Fatalf("got token %q, want the opposite", tok)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("%v: got %v, want %v", svc, got, want)
 		}
 	}
 }
@@ -198,6 +319,7 @@ func TestNextPageReflectionErrors(t *testing.T) {
 	}
 }
 
+// seq returns a slice containing the values in [from, to).
 func seq(from, to int) []int {
 	var r []int
 	for i := from; i < to; i++ {

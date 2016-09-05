9c9606bd3eae723a48064b375a28a240ada94c58
Dave Day <djd@golang.org>
2016-09-05 14:50:06 +1000

    iterator: give test output for examples
    Implement a nonsense prime iterator (based on sieve of Eratosthenes) and
    use it in the examples so that we can provide concrete output.
    
    The cute side effect of a prime seive is that it does
    exercise/demonstrate the idea that a requested page size differs from
    the number of returned elements. The iterator uses page sizes to
    determine how many numbers to consider, rather than how many to return.
    
    Change-Id: Ia087fab92a24d6b70288a059797788b363896736

--- iterator/examples_test.go
+++ iterator/examples_test.go
@@ -19,7 +19,10 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
+	"sort"
+	"strconv"
 
 	"golang.org/x/net/context"
 	"google.golang.org/api/iterator"
@@ -70,26 +73,56 @@ func Example_webHandler(w http.ResponseWriter, r *http.Request) {
 
 // This example demonstrates how to use a Pager to page through an iterator in a loop.
 func Example_pageLoop() {
-	const pageSize = 25
-	ctx := context.Background()
-	p := iterator.NewPager(client.Items(ctx), pageSize, "" /* start from the beginning */)
-	for {
+	// Ask for 3 pages of 5 item each from a Prime number iterator.
+	const pageSize = 5
+	p := iterator.NewPager(Primes(), pageSize, "" /* start from the beginning */)
+	for page := 0; page < 3; page++ {
 		var items []int
 		pageToken, err := p.NextPage(&items)
 		if err != nil {
-			// TODO: handle error.
+			log.Fatalf("Iterator paging failed: %v", err)
 		}
-		fmt.Println(items)
+		fmt.Printf("Page %d: %v\n", page, items)
 		if pageToken == "" {
 			break
 		}
 	}
+	// Output:
+	// Page 0: [2 3 5 7 11]
+	// Page 1: [13 17 19 23 29]
+	// Page 2: [31 37 41 43 47]
+	// Page 3: [53 59 61 67 71]
+	// Page 4: [73 79 83 89 97]
+	// Page 5: [101 103 107 109 113]
+	// Page 6: [127 131 137 139 149]
+	// Page 7: [151 157 163 167 173]
+	// Page 8: [179 181 191 193 197]
+	// Page 9: [199 211 223 227 229]
+}
+
+// The example demonstrates how to use a Pager to request a page from a given token.
+func Example_pageToken() {
+	const pageSize = 5
+	const pageToken = "1337"
+	p := iterator.NewPager(Primes(), pageSize, pageToken)
+
+	var items []int
+	nextPage, err := p.NextPage(&items)
+	if err != nil {
+		log.Fatalf("Iterator paging failed: %v", err)
+	}
+	fmt.Printf("Primes: %v\nToken:  %q\n", items, nextPage)
+	// Output:
+	// Primes: [1361 1367 1373 1381 1399]
+	// Token:  "1400"
 }
 
 // This example demonstrates how to get exactly the items in the buffer, without
 // triggering an extra RPC.
 func Example_serverPages() {
-	it := client.Items(ctx)
+	// The iterator returned by Primes has a default page size of 20, which means
+	// it will return all the primes in the range [2, 21).
+	it := Primes()
 	var items []int
 	for {
 		item, err := it.Next()
@@ -105,4 +138,86 @@ func Example_serverPages() {
 		}
 	}
 	fmt.Println(items)
+	// Output:
+	// [2 3 5 7 11 13 17 19]
+}
+
+// Primes returns a iterator which returns a sequence of prime numbers.
+func Primes() *SieveIterator {
+	it := &SieveIterator{pos: 2}
+	it.pageInfo, it.nextFunc = iterator.NewPageInfo(
+		it.fetch,
+		func() int { return len(it.items) },
+		func() interface{} { b := it.items; it.items = nil; return b })
+	return it
+}
+
+// SieveIterator is an iterator that returns primes using the sieve of
+// Eratosthenes. It is a demonstration of how an iterator might work.
+// Internally, it uses "page size" as the number of ints to consider,
+// and "page token" as the first number to consider (defaults to 2).
+type SieveIterator struct {
+	pageInfo *iterator.PageInfo
+	nextFunc func() error
+	p        []int // Primes in the range [2, pos).
+	pos      int   // Next number to consider when generating p.
+	items    []int
+}
+
+// PageInfo returns a PageInfo, which supports pagination.
+func (it *SieveIterator) PageInfo() *iterator.PageInfo { return it.pageInfo }
+
+func (it *SieveIterator) fetch(pageSize int, pageToken string) (string, error) {
+	start := 2
+	if pageToken != "" {
+		s, err := strconv.Atoi(pageToken)
+		if err != nil || s < 2 {
+			return "", fmt.Errorf("invalid token %q", pageToken)
+		}
+		start = s
+	}
+	if pageSize == 0 {
+		pageSize = 20 // Default page size.
+	}
+
+	// Make sure sufficient primes have been calculated.
+	it.calc(start + pageSize)
+
+	// Find the subslice of primes which match this page.
+	// Note that PageInfo requires that fetch does not remove any existing items,
+	// so we cannot assume that items is empty at this call.
+	items := it.p[sort.SearchInts(it.p, start):]
+	items = items[:sort.SearchInts(items, start+pageSize)]
+	it.items = append(it.items, items...)
+
+	return strconv.Itoa(start + pageSize), nil
+}
+
+// calc populates p with all primes up to, but not including, max.
+func (it *SieveIterator) calc(max int) {
+outer:
+	for x := it.pos; x < max; x++ {
+		sqrt := int(math.Sqrt(float64(x)))
+		for _, p := range it.p {
+			switch {
+			case x%p == 0:
+				// Not a prime.
+				continue outer
+			case p > sqrt:
+				// Only need to check up to sqrt.
+				break
+			}
+		}
+		it.p = append(it.p, x)
+	}
+	it.pos = max
+}
+
+func (it *SieveIterator) Next() (int, error) {
+	if err := it.nextFunc(); err != nil {
+		return 0, err
+	}
+	item := it.items[0]
+	it.items = it.items[1:]
+	return item, nil
 }

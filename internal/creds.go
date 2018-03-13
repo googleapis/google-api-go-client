1e5ebb0e4433d98a1e750e9a30504563064fa66a
Jonathan Amsterdam <jba@google.com>
2018-03-13 09:12:18 -0400

    internal: use google.CredentialsFromJSON
    Change-Id: I7b4524fa73fbc3041b203004689fc2fcf07f75b5
    Reviewed-on: https://code-review.googlesource.com/25071
    Reviewed-by: Chris Broadfoot <cbro@google.com>

--- internal/creds.go
+++ internal/creds.go
@@ -15,13 +15,10 @@
 package internal
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"time"
 
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
@@ -36,73 +33,10 @@ func Creds(ctx context.Context, ds *DialSettings) (*google.DefaultCredentials, e
 		if err != nil {
 			return nil, fmt.Errorf("cannot read credentials file: %v", err)
 		}
-		return credsFromJSON(ctx, data, ds.Scopes...)
+		return google.CredentialsFromJSON(ctx, data, ds.Scopes...)
 	}
 	if ds.TokenSource != nil {
 		return &google.DefaultCredentials{TokenSource: ds.TokenSource}, nil
 	}
 	return google.FindDefaultCredentials(ctx, ds.Scopes...)
-}
-
-// credFromJSON parses JSON for a refresh token or service account and returns
-// credentials constructed from it.
-// TODO(jba): move to oauth2/google.
-func credsFromJSON(ctx context.Context, data []byte, scope ...string) (*google.DefaultCredentials, error) {
-	// See if it is a refresh token credentials file first.
-	ts, ok, err := refreshTokenTokenSource(ctx, data, scope...)
-	if err != nil {
-		return nil, err
-	}
-	if ok {
-		return &google.DefaultCredentials{
-			TokenSource: ts,
-			JSON:        data,
-		}, nil
-	}
-
-	// If not, it should be a service account.
-	cfg, err := google.JWTConfigFromJSON(data, scope...)
-	if err != nil {
-		return nil, fmt.Errorf("google.JWTConfigFromJSON: %v", err)
-	}
-	// jwt.Config does not expose the project ID, so re-unmarshal to get it.
-	var pid struct {
-		ProjectID string `json:"project_id"`
-	}
-	if err := json.Unmarshal(data, &pid); err != nil {
-		return nil, err
-	}
-	return &google.DefaultCredentials{
-		ProjectID:   pid.ProjectID,
-		TokenSource: cfg.TokenSource(ctx),
-		JSON:        data,
-	}, nil
-}
-
-func refreshTokenTokenSource(ctx context.Context, data []byte, scope ...string) (oauth2.TokenSource, bool, error) {
-	var c cred
-	if err := json.Unmarshal(data, &c); err != nil {
-		return nil, false, fmt.Errorf("cannot unmarshal credentials file: %v", err)
-	}
-	if c.ClientID == "" || c.ClientSecret == "" || c.RefreshToken == "" || c.Type != "authorized_user" {
-		return nil, false, nil
-	}
-	cfg := &oauth2.Config{
-		ClientID:     c.ClientID,
-		ClientSecret: c.ClientSecret,
-		Endpoint:     google.Endpoint,
-		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
-		Scopes:       scope,
-	}
-	return cfg.TokenSource(ctx, &oauth2.Token{
-		RefreshToken: c.RefreshToken,
-		Expiry:       time.Now(),
-	}), true, nil
-}
-
-type cred struct {
-	ClientID     string `json:"client_id"`
-	ClientSecret string `json:"client_secret"`
-	RefreshToken string `json:"refresh_token"`
-	Type         string `json:"type"`
 }

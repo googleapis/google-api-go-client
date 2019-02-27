1949198e2e5ae38675b60302e6e08c5cf74493fb
Jean de Klerk <deklerk@google.com>
2019-02-27 15:14:17 -0700

    internal: improve CI/CD scripts
    - Codify CI/CD dependencies in go.mod / tools.go.
    - Use go mod tidy to check if deps were added without being added to go.mod.
    - Use go mod download to download deps instead of go get.
    - Re-pin grpc to v1.19.0 to fix cant-find-protobuf-package issue.
    - Use GO111MODULE=on everywhere applicable.
    - Switch to being 1.12 aware.
    
    Relevant internal bug: 123580802.
    
    Change-Id: I6976832f936e038eabdf35cdf79a901b73531e68
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/38611
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module google.golang.org/api
 require (
 	github.com/google/go-cmp v0.2.0
 	go.opencensus.io v0.18.0
+	golang.org/x/lint v0.0.0-20181026193005-c67002cb31c3
 	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f
+	golang.org/x/tools v0.0.0-20190114222345-bf090417da8b
 	google.golang.org/appengine v1.3.0
 	google.golang.org/genproto v0.0.0-20181202183823-bd91e49a0898
-	google.golang.org/grpc v1.17.0
+	google.golang.org/grpc v1.19.0
+	honnef.co/go/tools v0.0.0-20190102054323-c2f93a96b099
 )
--- internal/kokoro/vet.sh
+++ internal/kokoro/vet.sh
@@ -17,26 +17,25 @@
 # Fail on error, and display commands being run.
 set -ex
 
-# Only run the linter on go1.11, since it needs type aliases (and we only care about its output once).
-# TODO(deklerk) We should pass an environment variable from kokoro to decide this logic instead.
-if [[ `go version` != *"go1.11"* ]]; then
+# Only run the linter on go1.12, since it needs type aliases (and we only care
+# about its output once).
+if [[ `go version` != *"go1.12"* ]]; then
     exit 0
 fi
 
-pwd
-
-# Fail if a dependency was added without the necessary go.mod/go.sum change
-# being part of the commit.
-GO111MODULE=on go mod tidy
-git diff go.mod | tee /dev/stderr | (! read)
-git diff go.sum | tee /dev/stderr | (! read)
-
-try3() { eval "$*" || eval "$*" || eval "$*"; }
-
-try3 go get -u \
+go install \
   golang.org/x/lint/golint \
   golang.org/x/tools/cmd/goimports \
   honnef.co/go/tools/cmd/staticcheck
+
+# Fail if a dependency was added without the necessary go.mod/go.sum change
+# being part of the commit.
+go mod tidy
+git diff go.mod | tee /dev/stderr | (! read)
+git diff go.sum | tee /dev/stderr | (! read)
+
+# Easier to debug CI.
+pwd
 
 # Look at all .go files (ignoring .pb.go files) and make sure they have a Copyright. Fail if any don't.
 git ls-files "*[^.pb].go" | xargs grep -L "\(Copyright [0-9]\{4,\}\)" 2>&1 | tee /dev/stderr | (! read)

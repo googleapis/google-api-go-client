9e7f2a839bfefc34110ebbc321850d5a7ad1ebd4
Jean de Klerk <deklerk@google.com>
2019-04-15 11:29:09 -0600

    docs: clarify library stability
    Change-Id: I714e21760994a86590def1db2a2a8e5632409a00
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/39871
    Reviewed-by: Chris Broadfoot <cbro@google.com>

--- doc.go
+++ doc.go
@@ -27,10 +27,15 @@
 //
 // Versioning and Stability
 //
-// Clients in these subpackages follow semver as specified in
-// https://cloud.google.com/apis/design/versioning.
+// Due to the auto-generated nature of this collection of libraries, complete
+// APIs or specific versions can appear or go away without notice. As a result,
+// you should always locally vendor any API(s) that your code relies upon.
+//
+// Google APIs follow semver as specified by
+// https://cloud.google.com/apis/design/versioning. The code generator and
+// the code it produces - the libraries in the google.golang.org/api/...
+// subpackages - are beta.
 //
 // Note that versioning and stability is strictly not communicated through Go
-// modules. Go modules are used only for dependency management. See the link
-// above for stability guarantees.
+// modules. Go modules are used only for dependency management.
 package api

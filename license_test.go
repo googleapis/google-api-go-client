d63bd5fbc004a083dc85ef622366115e8088da62
Chris Cotter <cjcotter@google.com>
2019-10-21 12:53:15 -0400

    all: switch files to use LICENSE
    Switch all files to use the license from the LICENSE file, following
    guidance from legal. Also edit license test to only check for a
    single license.
    
    Fixes #414.
    
    Change-Id: I962a5c8b243e236881ef6f2b6ce8204715390cce
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/47230
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

--- license_test.go
+++ license_test.go
@@ -1,16 +1,6 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2019 Google LLC.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package api
 
@@ -23,28 +13,11 @@ import (
 	"testing"
 )
 
-// Files in this package use two separate licenses:
-
-// 1. A BSD license (used for generated files).
-var sentinelBSD = regexp.MustCompile(`// Copyright \d\d\d\d (Google LLC|The Go Authors)(\.)*( All rights reserved\.)*
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Files in this package use a BSD-style license.
+var sentinel = regexp.MustCompile(`(//|#) Copyright \d\d\d\d (Google LLC|The Go Authors)(\.)*( All rights reserved\.)*
+(//|#) Use of this source code is governed by a BSD-style
+(//|#) license that can be found in the LICENSE file.
 `)
-
-// 2. An Apache license (used for handwritten files).
-var sentinelApache = regexp.MustCompile(`(//|#) Copyright \d\d\d\d Google LLC
-(//|#)
-(//|#) Licensed under the Apache License, Version 2\.0 \(the "License"\);
-(//|#) you may not use this file except in compliance with the License\.
-(//|#) You may obtain a copy of the License at
-(//|#)
-(//|#)     https://www\.apache\.org/licenses/LICENSE-2\.0
-(//|#)
-(//|#) Unless required by applicable law or agreed to in writing, software
-(//|#) distributed under the License is distributed on an "AS IS" BASIS,
-(//|#) WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.
-(//|#) See the License for the specific language governing permissions and
-(//|#) limitations under the License\.`)
 
 const prefix = "// Copyright"
 
@@ -74,8 +47,8 @@ func TestLicense(t *testing.T) {
 			return nil
 		}
 
-		// Verify that one of the two valid licenses is matched.
-		if !sentinelBSD.Match(src) && !sentinelApache.Match(src) {
+		// Verify that the license is matched.
+		if !sentinel.Match(src) {
 			t.Errorf("%v: license header not present", path)
 			return nil
 		}

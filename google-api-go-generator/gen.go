1245eadc2b2824d97c7890a615108a3b321784cc
Jean de Klerk <deklerk@google.com>
2019-12-10 11:24:20 -0700

    youtube/v3: make EnableMonitorStream a *bool
    Without this change, "false" is interpreted as "empty" and does nothing. But, it
    is actually a meaningful value, meaning users have to find out how to use
    ForceSendFields. This CL changes it to trinary so that nil is empty and false is
    meaningful.
    
    Fixes #401
    
    Change-Id: I6c03f50614a901df6d7e75d0d1829d963cbe9890
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/49333
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Chris Broadfoot <cbro@google.com>

--- google-api-go-generator/gen.go
+++ google-api-go-generator/gen.go
@@ -918,6 +918,7 @@ var pointerFields = []fieldName{
 	{api: "storage:v1beta2", schema: "BucketLifecycleRuleCondition", field: "IsLive"},
 	{api: "tasks:v1", schema: "Task", field: "Completed"},
 	{api: "youtube:v3", schema: "ChannelSectionSnippet", field: "Position"},
+	{api: "youtube:v3", schema: "MonitorStreamInfo", field: "EnableMonitorStream"},
 }
 
 // forcePointerType reports whether p should be represented as a pointer type in its parent schema struct.

429e7e93c77a1a848a951c9335fd89c230c79f2f
Cody Oss <codyoss@google.com>
2020-03-18 13:17:07 -0600

    idtoken: support using ID Tokens
    This commit adds two new features to our apis that support the use
    of ID tokens.
    
    1. Users now have functions that fetch ID tokens for them:
      - NewClient
      - NewTokenSource
    Both of these are configured to auto-refresh tokens based off of
    Application Default Credentials or provided options.
    
    2. Users can now validate ID tokens with the following:
      - Validator.Validate
      - Validate
    These methods decode the provided token to figure out what kind of
    encrption was used to sign the token. This is then validated
    against the user's JWKs that are found at known Google endpoint.
    Because of this, these methods can only be used to validate Google
    ID tokens at this time. A common use case where a user would want
    to validate these tokens is if they are using App Eninge with IAP.
    
    Change-Id: I0877eff081b030eac55759444870c950cd028a14
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/53510
    Reviewed-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Chris Broadfoot <cbro@google.com>
    Reviewed-by: Tyler Bui-Palsulich <tbp@google.com>

---
+++ integration-tests/idtoken/idtoken_test.go
@@ -0,0 +1,47 @@
+// Copyright 2020 Google LLC.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build integration
+
+package idtoken
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/option"
+)
+
+const (
+	envCredentialFile = "API_GO_CLIENT_SA"
+	envTokenAudience  = "API_GO_CLIENT_TOKEN_AUDIENCE"
+)
+
+func TestNewTokenSource(t *testing.T) {
+	aud := os.Getenv(envTokenAudience)
+	ts, err := idtoken.NewTokenSource(context.Background(), aud, option.WithCredentialsFile(os.Getenv(envCredentialFile)))
+	if err != nil {
+		t.Fatalf("unable to create TokenSource: %v", err)
+	}
+	tok, err := ts.Token()
+	if err != nil {
+		t.Fatalf("unable to retrieve Token: %v", err)
+	}
+	req := &http.Request{Header: make(http.Header)}
+	tok.SetAuthHeader(req)
+	if !strings.HasPrefix(req.Header.Get("Authorization"), "Bearer ") {
+		t.Fatalf("token should sign requests with Bearer Authorization header")
+	}
+	validTok, err := idtoken.Validate(context.Background(), tok.AccessToken, aud)
+	if err != nil {
+		t.Fatalf("token validation failed: %v", err)
+	}
+	if validTok.Audience != aud {
+		t.Fatalf("got %q, want %q", validTok.Audience, aud)
+	}
+}

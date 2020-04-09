712b921eed77e92f573bf1190f7e2255cc642500
Cody Oss <codyoss@google.com>
2020-04-09 12:08:07 -0600

    examples: fix customSearchMain example
    Uncommented code and refactored according to the new api.
    
    Change-Id: I01c4252e01e8041ed35492c9cd140629258e9653
    Reviewed-on: https://code-review.googlesource.com/c/google-api-go-client/+/54551
    Reviewed-by: Chris Broadfoot <cbro@google.com>

--- examples/customsearch.go
+++ examples/customsearch.go
@@ -4,28 +4,36 @@
 
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	customsearch "google.golang.org/api/customsearch/v1"
+	"google.golang.org/api/googleapi/transport"
+)
+
 const (
 	apiKey = "some-api-key"
 	cx     = "some-custom-search-engine-id"
 	query  = "some-custom-query"
 )
 
-// TODO(codyoss): uncomment and refactor after generation.
 func customSearchMain() {
-	// client := &http.Client{Transport: &transport.APIKey{Key: apiKey}}
-	//
-	// svc, err := customsearch.New(client)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// resp, err := svc.Cse.Siterestrict.List(query).Cx(cx).Do()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// for i, result := range resp.Items {
-	// 	fmt.Printf("#%d: %s\n", i+1, result.Title)
-	// 	fmt.Printf("\t%s\n", result.Snippet)
-	// }
+	client := &http.Client{Transport: &transport.APIKey{Key: apiKey}}
+
+	svc, err := customsearch.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := svc.Cse.List().Q(query).Cx(cx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, result := range resp.Items {
+		fmt.Printf("#%d: %s\n", i+1, result.Title)
+		fmt.Printf("\t%s\n", result.Snippet)
+	}
 }

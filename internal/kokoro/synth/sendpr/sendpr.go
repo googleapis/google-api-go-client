226fd6999531dc4b6ae947dba9ec9f193c2f3b07
Chris Broadfoot <cbro@golang.org>
2020-05-21 13:24:44 -0700

    kokoro/synth/sendpr: fix compilation errors (#499)
    #489 had code that didn't compile (whoops), but strangely didn't get picked up by tests.

--- internal/kokoro/synth/sendpr/sendpr.go
+++ internal/kokoro/synth/sendpr/sendpr.go
@@ -1,5 +1,5 @@
-// Copyright 2020 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
+// Copyright 2020 Google LLC.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package main
@@ -28,9 +28,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	req := http.NewRequest("POST", "https://api.github.com/repos/googleapis/google-api-go-client/pulls", bytes.NewReader(body))
-	req.Headers.Set("Accept", "application/vnd.github.v3+json")
-	req.Headers.Set("Authorization", "token "+os.Getenv("GITHUB_TOKEN"))
+	req, err := http.NewRequest("POST", "https://api.github.com/repos/googleapis/google-api-go-client/pulls", bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("Authorization", "token "+os.Getenv("GITHUB_TOKEN"))
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -38,10 +41,10 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode > 299 {
-		log.Fatal("non-OK response creating PR: %s", body)
+		log.Fatalf("non-OK response creating PR: %s", bodyBytes)
 	}
 
-	log.Print("PR created: %s", body)
+	log.Printf("PR created: %s", bodyBytes)
 }

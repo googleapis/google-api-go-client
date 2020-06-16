84554e719a6818a8743ed7c3bb53e377251a282a
Chris Cotter <cjcotter@google.com>
2020-06-15 22:10:05 -0400

    fix build tag

--- internal/gensupport/retryable_linux.go
+++ internal/gensupport/retryable_linux.go
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux
+
+
 package gensupport
 
 import "syscall"
-
-// +build linux
 
 func init() {
 	// Initialize syscallRetryable to return true on transient socket-level

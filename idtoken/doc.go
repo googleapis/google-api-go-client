43c7148dea27928dab7f401acbd2382ed3eb621d
Cody Oss <codyoss@google.com>
2020-07-16 20:51:42 -0600

    chore(idtoken): fix some typos

--- idtoken/doc.go
+++ idtoken/doc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package idtoken provides utilities for creating authenticated transorts with
+// Package idtoken provides utilities for creating authenticated transports with
 // ID Tokens for Google HTTP APIs. It also provides methods to validate Google
 // issued ID tokens.
 package idtoken

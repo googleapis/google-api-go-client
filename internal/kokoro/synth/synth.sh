4b49df6a463e8401fc8a0ff9d05e5f261ecad363
Cody Oss <codyoss@google.com>
2020-09-10 09:51:31 -0600

    remove old version

--- internal/kokoro/synth/synth.sh
+++ internal/kokoro/synth/synth.sh
@@ -9,9 +9,10 @@ set -e -x
 cd $(dirname $0)/../../..
 
 # Install Go 1.15
-curl -o /tmp/go.tgz https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz && \
-    tar -C /usr/local -xzf /tmp/go.tgz && \
-    rm /tmp/go.tgz && \
+rm -rf /usr/local/go
+curl -o /tmp/go.tgz https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz &&
+    tar -C /usr/local -xzf /tmp/go.tgz &&
+    rm /tmp/go.tgz &&
     export PATH=$PATH:/usr/local/go/bin
 
 go version

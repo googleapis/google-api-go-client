18dadb13cc0a6e3d87ab5a0f2e4815a486921b9c
Cody Oss <codyoss@google.com>
2020-09-10 13:13:09 -0600

    tempdir

--- internal/kokoro/synth/synth.sh
+++ internal/kokoro/synth/synth.sh
@@ -9,10 +9,11 @@ set -e -x
 cd $(dirname $0)/../../..
 
 # Install Go 1.15
+tempdir=$(mktemp)
 curl -o /tmp/go.tgz https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz &&
-    tar -C $HOME -xzf /tmp/go.tgz &&
+    tar -C tempdir -xzf /tmp/go.tgz &&
     rm /tmp/go.tgz &&
-    export PATH=$HOME/go/bin:$PATH
+    export PATH=$tempdir/go/bin:$PATH
 
 go version
 go env

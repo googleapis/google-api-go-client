3f0b0482466873fbf5966e11934e134597c136dc
Cody Oss <codyoss@google.com>
2020-09-10 14:17:51 -0600

    chore(internal/kokoro): fix var typo

--- internal/kokoro/synth/synth.sh
+++ internal/kokoro/synth/synth.sh
@@ -11,7 +11,7 @@ cd $(dirname $0)/../../..
 # Install Go 1.15
 tempdir=$(mktemp)
 curl -o /tmp/go.tgz https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz &&
-    tar -C tempdir -xzf /tmp/go.tgz &&
+    tar -C $tempdir -xzf /tmp/go.tgz &&
     rm /tmp/go.tgz &&
     export PATH=$tempdir/go/bin:$PATH
 

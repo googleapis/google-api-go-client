86c3b801ca38eefb0ab5d56c4bf88c193ca9fb2b
Cody Oss <codyoss@google.com>
2021-03-15 10:33:08 -0600

    feat: add impersonate pacakge
    This pacakge is used for creating a impersonated TokenSource. This
    TokenSource can then be passed into any client or API that accepts
    a client option. This package provides support for three types of
    impersonation:
    
    1. A service account impersonating another service account.
    2. A service account impersonating another service account as an
       admin user -- a pattern used with domain wide delegation.
    3. A service account creating an impersonated ID token.

---
+++ impersonate/integration_test.go
@@ -0,0 +1,145 @@
+// Copyright 2021 Google LLC.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package impersonate_test
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/api/impersonate"
+	"google.golang.org/api/option"
+
+	"google.golang.org/api/storage/v1"
+)
+
+var (
+	baseKeyFile   string
+	readerKeyFile string
+	readerEmail   string
+	writerEmail   string
+	projectID     string
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+	baseKeyFile = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	readerKeyFile = os.Getenv("GCLOUD_TESTS_IMPERSONATE_READER_KEY")
+	readerEmail = os.Getenv("GCLOUD_TESTS_IMPERSONATE_READER_EMAIL")
+	writerEmail = os.Getenv("GCLOUD_TESTS_IMPERSONATE_WRITER_EMAIL")
+	os.Exit(m.Run())
+}
+
+func TestTokenSourceIntegration(t *testing.T) {
+	if !testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	ctx := context.Background()
+	tests := []struct {
+		name        string
+		baseKeyFile string
+		delgates    []string
+	}{
+		{
+			name:        "SA -> SA",
+			baseKeyFile: readerKeyFile,
+		},
+		{
+			name:        "SA -> Delegate -> SA",
+			baseKeyFile: baseKeyFile,
+			delgates:    []string{readerEmail},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts, err := impersonate.TokenSource(ctx,
+				impersonate.Config{
+					TargetPrincipal: writerEmail,
+					Scopes:          []string{"https://www.googleapis.com/auth/devstorage.full_control"},
+					Delegates:       tt.delgates,
+				},
+				option.WithCredentialsFile(tt.baseKeyFile),
+			)
+			if err != nil {
+				t.Fatalf("failed to create ts: %v", err)
+			}
+			svc, err := storage.NewService(ctx, option.WithTokenSource(ts))
+			if err != nil {
+				t.Fatalf("failed to create client: %v", err)
+			}
+			bucketName := fmt.Sprintf("%s-%d", projectID, rand.Int63())
+			if _, err := svc.Buckets.Insert(projectID, &storage.Bucket{
+				Name: bucketName,
+			}).Do(); err != nil {
+				t.Fatalf("error creating bucket: %v", err)
+			}
+			if err := svc.Buckets.Delete(bucketName).Do(); err != nil {
+				t.Fatalf("unable to cleanup bucket %q: %v", bucketName, err)
+			}
+		})
+	}
+}
+
+func TestIDTokenSourceIntegration(t *testing.T) {
+	if !testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	ctx := context.Background()
+	tests := []struct {
+		name        string
+		baseKeyFile string
+		delgates    []string
+	}{
+		{
+			name:        "SA -> SA",
+			baseKeyFile: readerKeyFile,
+		},
+		{
+			name:        "SA -> Delegate -> SA",
+			baseKeyFile: baseKeyFile,
+			delgates:    []string{readerEmail},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			aud := "http://example.com/"
+			ts, err := impersonate.IDTokenSource(ctx,
+				impersonate.IDTokenConfig{
+					TargetPrincipal: writerEmail,
+					Audience:        aud,
+					Delegates:       tt.delgates,
+					IncludeEmail:    true,
+				},
+				option.WithCredentialsFile(tt.baseKeyFile),
+			)
+			if err != nil {
+				t.Fatalf("failed to create ts: %v", err)
+			}
+			tok, err := ts.Token()
+			if err != nil {
+				t.Fatalf("unable to retrieve Token: %v", err)
+			}
+			validTok, err := idtoken.Validate(context.Background(), tok.AccessToken, aud)
+			if err != nil {
+				t.Fatalf("token validation failed: %v", err)
+			}
+			if validTok.Audience != aud {
+				t.Fatalf("got %q, want %q", validTok.Audience, aud)
+			}
+			if validTok.Claims["email"] != writerEmail {
+				t.Fatalf("got %q, want %q", validTok.Claims["email"], writerEmail)
+			}
+		})
+	}
+}
---
+++ impersonate/user.go
@@ -0,0 +1,179 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package impersonate
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+func user(ctx context.Context, c Config, client *http.Client) (oauth2.TokenSource, error) {
+	// Default to the longest acceptable value of one hour as the token will
+	// be refreshed automatically if not set.
+	lifetime := 3600 * time.Second
+	if c.Lifetime != 0 {
+		lifetime = c.Lifetime
+	}
+	u := userTokenSource{
+		client:          client,
+		targetPrincipal: c.TargetPrincipal,
+		subject:         c.Subject,
+		lifetime:        lifetime,
+	}
+	u.delegates = make([]string, len(c.Delegates))
+	for i, v := range c.Delegates {
+		u.delegates[i] = formatIAMServiceAccountName(v)
+	}
+	u.scopes = make([]string, len(c.Scopes))
+	copy(u.scopes, c.Scopes)
+	// Don't auto-refresh token if a lifetime is configured.
+	if c.Lifetime != 0 {
+		tok, err := u.Token()
+		if err != nil {
+			return nil, err
+		}
+		return oauth2.StaticTokenSource(tok), nil
+	}
+	return oauth2.ReuseTokenSource(nil, u), nil
+}
+
+type claimSet struct {
+	Iss   string `json:"iss"`
+	Scope string `json:"scope,omitempty"`
+	Sub   string `json:"sub,omitempty"`
+	Aud   string `json:"aud"`
+	Iat   int64  `json:"iat"`
+	Exp   int64  `json:"exp"`
+}
+
+type signJWTRequest struct {
+	Payload   string   `json:"payload"`
+	Delegates []string `json:"delegates,omitempty"`
+}
+
+type signJWTResponse struct {
+	// KeyID is the key used to sign the JWT.
+	KeyID string `json:"keyId"`
+	// SignedJwt contains the automatically generated header; the
+	// client-supplied payload; and the signature, which is generated using
+	// the key referenced by the `kid` field in the header.
+	SignedJWT string `json:"signedJwt"`
+}
+
+type exchangeTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+type userTokenSource struct {
+	client  *http.Client
+	nowFunc func() time.Time
+
+	targetPrincipal string
+	subject         string
+	scopes          []string
+	lifetime        time.Duration
+	delegates       []string
+}
+
+func (u userTokenSource) Token() (*oauth2.Token, error) {
+	signedJWT, err := u.signJWT()
+	if err != nil {
+		return nil, err
+	}
+	return u.exchangeToken(signedJWT)
+}
+
+func (u userTokenSource) signJWT() (string, error) {
+	now := u.now()
+	exp := now.Add(u.lifetime)
+	claims := claimSet{
+		Iss:   u.targetPrincipal,
+		Scope: strings.Join(u.scopes, " "),
+		Sub:   u.subject,
+		Aud:   fmt.Sprintf("%s/token", oauth2Endpoint),
+		Iat:   now.Unix(),
+		Exp:   exp.Unix(),
+	}
+	payloadBytes, err := json.Marshal(claims)
+	if err != nil {
+		return "", fmt.Errorf("impersonate: unable to marshal claims: %v", err)
+	}
+	signJWTReq := signJWTRequest{
+		Payload:   string(payloadBytes),
+		Delegates: u.delegates,
+	}
+
+	bodyBytes, err := json.Marshal(signJWTReq)
+	if err != nil {
+		return "", fmt.Errorf("impersonate: unable to marshal request: %v", err)
+	}
+	reqURL := fmt.Sprintf("%s/v1/%s:signJwt", iamCredentailsEndpoint, formatIAMServiceAccountName(u.targetPrincipal))
+	req, err := http.NewRequest("POST", reqURL, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return "", fmt.Errorf("impersonate: unable to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rawResp, err := u.client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("impersonate: unable to sign JWT: %v", err)
+	}
+	body, err := ioutil.ReadAll(io.LimitReader(rawResp.Body, 1<<20))
+	if err != nil {
+		return "", fmt.Errorf("impersonate: unable to read body: %v", err)
+	}
+	if c := rawResp.StatusCode; c < 200 || c > 299 {
+		return "", fmt.Errorf("impersonate: status code %d: %s", c, body)
+	}
+
+	var signJWTResp signJWTResponse
+	if err := json.Unmarshal(body, &signJWTResp); err != nil {
+		return "", fmt.Errorf("impersonate: unable to parse response: %v", err)
+	}
+	return signJWTResp.SignedJWT, nil
+}
+
+func (u userTokenSource) exchangeToken(signedJWT string) (*oauth2.Token, error) {
+	v := url.Values{}
+	v.Set("grant_type", "assertion")
+	v.Set("assertion_type", "http://oauth.net/grant_type/jwt/1.0/bearer")
+	v.Set("assertion", signedJWT)
+	rawResp, err := u.client.PostForm(fmt.Sprintf("%s/token", oauth2Endpoint), v)
+	if err != nil {
+		return nil, fmt.Errorf("impersonate: unable to exchange token: %v", err)
+	}
+	body, err := ioutil.ReadAll(io.LimitReader(rawResp.Body, 1<<20))
+	if err != nil {
+		return nil, fmt.Errorf("impersonate: unable to read body: %v", err)
+	}
+	var tokenResp exchangeTokenResponse
+	if err := json.Unmarshal(body, &tokenResp); err != nil {
+		return nil, fmt.Errorf("impersonate: unable to parse response: %v", err)
+	}
+
+	return &oauth2.Token{
+		AccessToken: tokenResp.AccessToken,
+		TokenType:   tokenResp.TokenType,
+		Expiry:      u.now().Add(time.Second * time.Duration(tokenResp.ExpiresIn)),
+	}, nil
+}
+
+func (u userTokenSource) now() time.Time {
+	if u.nowFunc == nil {
+		return time.Now()
+	}
+	return u.nowFunc()
+}

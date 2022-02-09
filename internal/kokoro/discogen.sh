96ffe98eaa07fa5b9b91de91557b45106df24dcf
Cody Oss <codyoss@google.com>
2022-02-09 10:30:45 -0700

    chore: add a script that invoke regen
    This will be called from a cron job. Script should land before
    backing config. Related cl: 427492527

---
+++ internal/kokoro/discogen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright 2022 Google LLC.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Fail on any error
+set -eo pipefail
+
+export GITHUB_ACCESS_TOKEN=$(cat $KOKORO_KEYSTORE_DIR/73713_yoshi-automation-github-key)
+
+# Display commands being run
+set -x
+
+# cd to project dir on Kokoro instance
+cd github/google-api-go-client
+export DISCOVERY_DIR=$(pwd)
+
+go run google.golang.org/api/internal/kokoro/discogen

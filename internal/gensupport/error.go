dbc3330e62f5bf9553e4f24074be09df3e9cf9b7
Chris Smith <chrisdsmith@google.com>
2022-11-03 16:29:36 -0600

    fix function docs and copyright year

--- internal/gensupport/error.go
+++ internal/gensupport/error.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC. All rights reserved.
+// Copyright 2022 Google LLC. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -11,10 +11,11 @@ import (
 	"google.golang.org/api/googleapi"
 )
 
-// WrapError creates an apierror.APIError from err (one that
-// does not wrap err), wraps it in err, and returns err. If
-// err is not a googleapi.Error (or a gRPC Status), it returns
-// err without modification.
+// WrapError creates an [apierror.APIError] from err (one that does not wrap
+// err), wraps it in err, and returns err. If err is not a [googleapi.Error]
+// (or a gRPC [Status]), it returns err without modification.
+//
+// [Status]: https://pkg.go.dev/google.golang.org/grpc/status#Status
 func WrapError(err error) error {
 	if apiError, ok := apierror.ParseError(err, false); ok {
 		var herr *googleapi.Error

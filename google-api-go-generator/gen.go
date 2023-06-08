71a3015a206d981eef3b20f70b1ae19659501367
Chris Smith <chrisdsmith@google.com>
2023-06-08 15:05:08 -0600

    fix formatting

--- google-api-go-generator/gen.go
+++ google-api-go-generator/gen.go
@@ -68,7 +68,7 @@ var (
 // skipAPIGeneration is a set of APIs to not generate when generating all clients.
 var skipAPIGeneration = map[string]bool{
 	"integrations:v1": true,
-	"sql:v1beta4": true,
+	"sql:v1beta4":     true,
 }
 
 // API represents an API to generate, as well as its state while it's

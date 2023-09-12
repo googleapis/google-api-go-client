c881dfec32667f8ffafde782c875b8009a7a820e
Chris Smith <chrisdsmith@google.com>
2023-09-12 15:20:33 -0600

    fix(transport): remove conditional App Engine gen 1 Go hooks
    * Remove gRPC hook for google.golang.org/appengine/socket
    * Remove HTTP hook for google.golang.org/appengine/urlfetch
    
    Both hooks load packages that are no longer intended for use in the
    App Engine Go second generation runtime (>= Go 1.11).
    
    closes: #2128

--- transport/grpc/dial_test.go
+++ transport/grpc/dial_test.go
@@ -5,54 +5,8 @@
 package grpc
 
 import (
-	"context"
-	"errors"
-	"net"
 	"testing"
-	"time"
-
-	"golang.org/x/oauth2"
-	"google.golang.org/api/option"
-	"google.golang.org/grpc"
 )
-
-// Check that user optioned grpc.WithDialer option overwrites App Engine dialer
-func TestGRPCHook(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-	expected := false
-
-	appengineDialerHook = (func(ctx context.Context) grpc.DialOption {
-		return grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
-			t.Error("did not expect a call to appengine dialer, got one")
-			cancel()
-			return nil, errors.New("not expected")
-		})
-	})
-	defer func() {
-		appengineDialerHook = nil
-	}()
-
-	expectedDialer := grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
-		expected = true
-		cancel()
-		return nil, errors.New("expected")
-	})
-
-	conn, err := Dial(ctx,
-		option.WithTokenSource(oauth2.StaticTokenSource(nil)), // No creds.
-		option.WithGRPCDialOption(expectedDialer),
-		option.WithGRPCDialOption(grpc.WithBlock()))
-	if err != context.Canceled {
-		t.Errorf("got %v, want %v", err, context.Canceled)
-	}
-	if conn != nil {
-		conn.Close()
-		t.Error("got valid conn, want nil")
-	}
-	if !expected {
-		t.Error("expected a call to expected dialer, didn't get one")
-	}
-}
 
 func TestCheckDirectPathEndPoint(t *testing.T) {
 	for _, testcase := range []struct {

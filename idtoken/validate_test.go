d541d8e4f6699db6216a0aeee5a7a5c23ed8a3b7
Eytan Kidron <eytankidron@google.com>
2023-09-14 15:30:36 -0400

    feat(idtoken): add ParsePayload returning unvalidated token payload (#2136)

--- idtoken/validate_test.go
+++ idtoken/validate_test.go
@@ -231,6 +231,39 @@ func TestValidateES256(t *testing.T) {
 	}
 }
 
+func TestParsePayload(t *testing.T) {
+	idToken, _ := createRS256JWT(t)
+	tests := []struct {
+		name                string
+		token               string
+		wantPayloadAudience string
+		wantErr             bool
+	}{{
+		name:                "valid token",
+		token:               idToken,
+		wantPayloadAudience: testAudience,
+	}, {
+		name:    "unparseable token",
+		token:   "aaa.bbb.ccc",
+		wantErr: true,
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			payload, err := ParsePayload(tt.token)
+			gotErr := err != nil
+			if gotErr != tt.wantErr {
+				t.Errorf("ParsePayload(%q) got error %v, wantErr = %v", tt.token, err, tt.wantErr)
+			}
+			if tt.wantPayloadAudience != "" {
+				if payload == nil || payload.Audience != tt.wantPayloadAudience {
+					t.Errorf("ParsePayload(%q) got payload %+v, want payload with audience = %q", tt.token, payload, tt.wantPayloadAudience)
+				}
+			}
+		})
+	}
+}
+
 func createES256JWT(t *testing.T) (string, ecdsa.PublicKey) {
 	t.Helper()
 	token := commonToken(t, "ES256")

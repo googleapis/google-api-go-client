69626e37973a7f1e7974d7dd0e6e0fee3c0ebe6f
Chris Smith <chrisdsmith@google.com>
2024-01-19 15:30:36 -0700

    feat(transport): add universe domain support (#2355)

--- transport/http/dial_test.go
+++ transport/http/dial_test.go
@@ -11,6 +11,8 @@ import (
 
 	"go.opencensus.io/plugin/ochttp"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 )
 
 func TestNewClient(t *testing.T) {
@@ -35,5 +37,19 @@ func TestNewClient(t *testing.T) {
 	t2 := t1.Base.(*ochttp.Transport)
 	if got, want := fmt.Sprintf("%T", t2.Base), "*otelhttp.Transport"; got != want {
 		t.Fatalf("got %s, want: %s", got, want)
+	}
+}
+
+func TestNewClient_MismatchedUniverseDomainCreds(t *testing.T) {
+	rootTokenScope := "https://www.googleapis.com/auth/cloud-platform"
+	universeDomain := "example.com"
+	universeDomainDefault := "googleapis.com"
+	creds := &google.Credentials{} // universeDomainDefault
+	wantErr := errUniverseNotMatch(universeDomain, universeDomainDefault)
+	_, _, err := NewClient(context.Background(), option.WithUniverseDomain(universeDomain),
+		option.WithCredentials(creds), option.WithScopes(rootTokenScope))
+
+	if err.Error() != wantErr.Error() {
+		t.Fatalf("got: %v, want: %v", err, wantErr)
 	}
 }

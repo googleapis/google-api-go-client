040476970a74b199e79c01d5584793a8c6462091
Chris Smith <chrisdsmith@google.com>
2024-02-05 14:22:55 -0700

    simplify select by removing for loop

--- internal/settings.go
+++ internal/settings.go
@@ -208,32 +208,21 @@ func GetUniverseDomain(creds *google.Credentials) (string, error) {
 		results <- result
 	}()
 
-loop:
-	for {
-		select {
-		case <-timer.C: // Timer is expired.
-			break loop
-		default: // Timer is not expired, so continue.
-		}
-
-		select {
-		case err := <-errors:
-			// An error returned before the timer expires is legitimate.
-			return "", err
-		case res := <-results:
-			return res, nil
-		case <-time.After(time.Millisecond * 100): // Poll channels every 100ms.
-		}
-
+	select {
+	case err := <-errors:
+		// An error that is returned before the timer expires is legitimate.
+		return "", err
+	case res := <-results:
+		return res, nil
+	case <-timer.C: // Timer is expired.
+		// If err or res was not returned, it means that creds.GetUniverseDomain()
+		// did not complete in 1s. Assume that MDS is likely never responding to
+		// the endpoint and will timeout. This is the source of issues such as
+		// https://github.com/googleapis/google-cloud-go/issues/9350.
+		// Temporarily (2024-02-02) return the GDU domain. Restore the original
+		// calls to creds.GetUniverseDomain() in grpc/dial.go and http/dial.go
+		// and remove this method to close
+		// https://github.com/googleapis/google-api-go-client/issues/2399.
+		return universeDomainDefault, nil
 	}
-
-	// If result or err was never returned, it means that creds.GetUniverseDomain()
-	// did not complete in 1s. Assume that MDS is likely never responding to
-	// the endpoint and will timeout. This is the source of issues such as
-	// https://github.com/googleapis/google-cloud-go/issues/9350.
-	// Temporarily (2024-02-02) return the GDU domain. Restore the original
-	// calls to creds.GetUniverseDomain() in grpc/dial.go and http/dial.go
-	// and remove this method to close
-	// https://github.com/googleapis/google-api-go-client/issues/2399.
-	return universeDomainDefault, nil
 }

2a5c807a86d2712d685e06f59cd5d25740b46c71
Chris Smith <chrisdsmith@google.com>
2026-01-22 12:16:55 -0700

    fix(internaloption): add WithTelemetryAttributes (#3442)

--- option/internaloption/internaloption_test.go
+++ option/internaloption/internaloption_test.go
@@ -63,3 +63,19 @@ func TestDefaultApply(t *testing.T) {
 		t.Error(cmp.Diff(got, want, ignore...))
 	}
 }
+
+func TestWithTelemetryAttributes(t *testing.T) {
+	attrs := map[string]string{"foo": "bar"}
+	opt := WithTelemetryAttributes(attrs)
+	var ds internal.DialSettings
+	opt.Apply(&ds)
+	if diff := cmp.Diff(attrs, ds.TelemetryAttributes); diff != "" {
+		t.Errorf("TelemetryAttributes mismatch (-want +got):\n%s", diff)
+	}
+
+	// Verify cloning
+	attrs["foo"] = "baz"
+	if ds.TelemetryAttributes["foo"] != "bar" {
+		t.Errorf("TelemetryAttributes was not cloned: got %q, want %q", ds.TelemetryAttributes["foo"], "bar")
+	}
+}

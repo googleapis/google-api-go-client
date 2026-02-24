1c1d837a7acf381b3a74234f0147df1f7e95f413
Seth Hollyman <shollyman@google.com>
2026-02-24 18:23:13 +0000

    feat(option/internaloption): unsafe resolver

---
+++ option/internaloption/unsaferesolver.go
@@ -0,0 +1,46 @@
+// Copyright 2026 Google LLC.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package internaloption
+
+import (
+	"errors"
+
+	"google.golang.org/api/internal"
+	"google.golang.org/api/option"
+)
+
+type unsafeResolver struct {
+	ds *internal.DialSettings
+}
+
+// NewUnsafeResolver provides and experimental mechanism for introspecting
+// passed-in ClientOptions.  It is intended for use by internal Google client
+// code, and provides no stability contract as it's dependent on internal
+// implementation details.
+//
+// The method is experimental and subject to change without notice.
+func NewUnsafeResolver(opts ...option.ClientOption) *unsafeResolver {
+	ds := new(internal.DialSettings)
+	for _, o := range opts {
+		o.Apply(ds)
+	}
+	return &unsafeResolver{
+		ds: ds,
+	}
+}
+
+// ResolvedGRPCConnPoolSize provides the passed in value correspnding to the
+// WithGRPCConnectionPool option in google.golang.org/option.
+func (ur *unsafeResolver) ResolvedGRPCConnPoolSize() int {
+	return ur.ds.GRPCConnPoolSize
+}
+
+// ProbeDirectPathConfiguration attempts to identify if the passed in options
+// are expected to yield result in a client that can communicate over direct
+// path.  It will return an error with more details about possible causes that
+// prevent direct path from being used, or nil if no issues were identified.
+func (ur *unsafeResolver) ProbeDirectPathConfiguration() error {
+	return errors.New("unimplemented")
+}

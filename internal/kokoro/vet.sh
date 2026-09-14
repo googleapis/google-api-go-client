f8b7bf441c9e2e8ce8ff825b3767f26a594e029a
Seth Hollyman <shollyman@google.com>
2026-09-14 16:31:26 +0000

    testing: refine vet checks
    This PR alters how we handle code analysis.  Instead of running
    staticcheck on latest and targeting earliest, we simply run it on
    earliest.
    
    We now only do the go mod diff / format checks on latest.
    
    This PR also removes the now deprecated golint usage.
    
    Finally, this PR handles a minor style warning from vet.

--- internal/kokoro/vet.sh
+++ internal/kokoro/vet.sh
@@ -7,43 +7,28 @@
 # Fail on error, and display commands being run.
 set -ex
 
+# Only do the formatting and mod checking for the "latest" version
 if [[ $KOKORO_JOB_NAME != *"latest-version"* ]]; then
-  exit 0
+  # Fail if a dependency was added without the necessary go.mod/go.sum change
+  # being part of the commit.
+  go mod tidy
+  git diff go.mod | tee /dev/stderr | (! read)
+  git diff go.sum | tee /dev/stderr | (! read)
+
+  # Easier to debug CI.
+  pwd
+
+  gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
+  goimports -l . 2>&1 | tee /dev/stderr | (! read)
+
 fi
 
-# Fail if a dependency was added without the necessary go.mod/go.sum change
-# being part of the commit.
-go mod tidy
-git diff go.mod | tee /dev/stderr | (! read)
-git diff go.sum | tee /dev/stderr | (! read)
-
-# Easier to debug CI.
-pwd
-
-gofmt -s -d -l . 2>&1 | tee /dev/stderr | (! read)
-goimports -l . 2>&1 | tee /dev/stderr | (! read)
-
-# Runs the linter. Regrettably the linter is very simple and does not provide the ability to exclude rules or files,
-# so we rely on inverse grepping to do this for us.
-golint ./... 2>&1 | (
-  grep -v "gen.go" |
-    grep -v "disco.go" |
-    grep -v "exported const DefaultDelayThreshold should have comment" |
-    grep -v "exported const DefaultBundleCountThreshold should have comment" |
-    grep -v "exported const DefaultBundleByteThreshold should have comment" |
-    grep -v "exported const DefaultBufferedByteLimit should have comment" |
-    grep -v "error var Done should have name of the form ErrFoo" |
-    grep -v "exported method APIKey.RoundTrip should have comment or be unexported" |
-    grep -v "exported method MarshalStyle.JSONReader should have comment or be unexported" |
-    grep -v "UnmarshalJSON should have comment or be unexported" |
-    grep -v "MarshalJSON should have comment or be unexported" |
-    grep -v ".Apply should have comment or be unexported" |
-    grep -vE "\.pb\.go:" || true
-) | tee /dev/stderr | (! read)
-
-staticcheck -go 1.25 ./... 2>&1 | (
-  grep -v "SA1019" |
-    grep -v "S1007" |
-    grep -v "error var Done should have name of the form ErrFoo" |
-    grep -v "examples" || true
-) | tee /dev/stderr | (! read)
+# Only do staticcheck on our "earliest" version.
+if [[ $KOKORO_JOB_NAME != *"earliest-version"* ]]; then
+  staticcheck ./... 2>&1 | (
+    grep -v "SA1019" |
+      grep -v "S1007" |
+      grep -v "error var Done should have name of the form ErrFoo" |
+      grep -v "examples" || true
+  ) | tee /dev/stderr | (! read)
+fi
